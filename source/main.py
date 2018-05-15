40c8f88af93a49dd37cd99a7d671d337d3846454
Forbrig <vitorforbrig@gmail.com>
2018-05-15 00:41:08 -0300

    changed the shape of the map and player

--- source/main.py
+++ source/main.py
@@ -14,26 +14,21 @@ clock = pygame.time.Clock()
 #    x = display_width
 #    y = display_height
 
-player_pos = [50, 300 + (25/2)]
-player_velo = 20
+player_pos = [100, 100]
+player_velo = 25
 
-left_point = (50, 325)
-right_point = (750, 325)
-upper_point = (400, 150)
-bottom_point = (400, 500)
+left_up = (100, 100)
+right_up = (100, 500)
+left_bottom = (700, 100)
+right_bottom = (700, 500)
 
 running = True
 
 while running:
-    #screen.blit(, player_pos)
-
     screen.fill(0)
-    #pygame.draw.polygon(screen, (50, 30, 30), ((50, 350), (50, 370), (400, 520), (750, 370), (750, 350)), 0)
-    pygame.draw.line(screen, (70, 50, 50), (50, 335), (400, 510), 20) # left soil
-    pygame.draw.line(screen, (50, 30, 30), (400, 510), (750, 335), 20) # right soil
-    pygame.draw.polygon(screen, (30, 160, 30), (left_point, upper_point, right_point, bottom_point, left_point), 0) # terrain
-
-    pygame.draw.polygon(screen, (255, 0, 0), ((0 + player_pos[0], (25/2) + player_pos[1]), (25 + player_pos[0], 0 + player_pos[1]), (50 + player_pos[0], (25/2) + player_pos[1]), (25 + player_pos[0], (25) + player_pos[1]), (0 + player_pos[0], (25/2) + player_pos[1])), 0)
+    pygame.draw.line(screen, (50, 30, 30), (100, 510), (700, 510), 20) # bottom soil
+    pygame.draw.polygon(screen, (30, 160, 30), (left_up, left_bottom, right_bottom, right_up, left_up), 0) # terrain
+    pygame.draw.polygon(screen, (255, 0, 0), ((0 + player_pos[0], 0 + player_pos[1]), (0 + player_pos[0], 25 + player_pos[1]), (25 + player_pos[0], 25 + player_pos[1]), (25 + player_pos[0], 0 + player_pos[1]), (0 + player_pos[0], 0 + player_pos[1])), 0)
 
 
     for event in pygame.event.get():
