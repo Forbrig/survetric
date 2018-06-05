c8df67b0188b84a71de3d2d2626a4de3812f03f7
Vitor Forbrig <vitorforbrig@gmail.com>
2018-06-05 18:42:23 -0300

    map bord colision, life bar, fps count

--- source/main.py
+++ source/main.py
@@ -2,10 +2,16 @@ import pygame
 
 class player(object):
     def __init__(self):
+        self.life = 5
         self.x = 100
         self.y = 100
 
     def draw(self):
+        #drawing lifes
+        for i in range(self.life):
+            step = i * 20
+            pygame.draw.polygon(screen, (255, 0, 0), ((20 + step, 20), (20 + step, 30), (30 + step, 30), (30 + step, 20), (20 + step, 20)), 0)
+        #drawing player
         pygame.draw.polygon(screen, (255, 0, 0), ((self.x, self.y), (self.x, 25 + self.y), (25 + self.x, 25 + self.y), (25 + self.x, self.y), (self.x, self.y)), 0)
 
     def move(self, speed_x, speed_y):
@@ -20,6 +26,8 @@ screenDimension = (display_width, display_height)
 screen = pygame.display.set_mode(screenDimension, 0, 32)
 pygame.display.set_caption("survetric")
 clock = pygame.time.Clock()
+pygame.init()
+font = pygame.font.Font(None, 30)
 
 left_up = (100, 100)
 right_up = (100, 500)
@@ -33,6 +41,11 @@ speed_y = 0
 running = True
 while running:
     screen.fill(0)
+
+    #render fps in the top right
+    fps = font.render(str(int(clock.get_fps())), True, pygame.Color('green'))
+    screen.blit(fps, (770, 5))
+
     pygame.draw.line(screen, (50, 30, 30), (120, 510), (700, 510), 20) # bottom soil
     pygame.draw.polygon(screen, (50, 30, 30), ((100, 500), (120, 500), (120, 520)), 0)
     pygame.draw.polygon(screen, (50, 30, 30), ((700, 500), (700, 520), (720, 520)), 0)
@@ -68,7 +81,8 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    player.move(speed_x, speed_y)
+    if player.x + speed_x >= 100 and player.x + speed_x < 700 and player.y + speed_y >= 100 and player.y + speed_y < 500:
+        player.move(speed_x, speed_y)
 
     pygame.display.update()
     clock.tick(60)
